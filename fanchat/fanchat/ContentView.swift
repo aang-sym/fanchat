d6a40ca79f720856f867de9aa92bf0d863e7a327
Angus Symons <a_sym@Anguss-MacBook-Pro.local>
2022-09-21 17:01:16 +1000

    first xcode commit

---
+++ fanchat/fanchat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  fanchat
+//
+//  Created by Angus Symons on 21/9/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fanchat/fanchat/fanchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  fanchatApp.swift
+//  fanchat
+//
+//  Created by Angus Symons on 21/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct fanchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
